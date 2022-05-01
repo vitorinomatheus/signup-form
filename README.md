14e4835358d96be7a9505b015736688360ff58bb
vitorinomatheus <matheusvs122@gmail.com>
2022-04-30 22:34:20 -0300

    add valid/invalid style, phone mask and confirm password function

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-#singup-form
+#signup-form
 
-This project's goal is to build a sing-up page model. It's theme is a made-up chess championship.
+This project's goal is to build a sign-up page model. It's theme is a made-up chess championship.
+
+1 - I used flexblox to organize the overall layout, setting logo and bg-image on the left, and user's interaction space on the right;
+2 - Relative positioning was used to adjust logo height;
+3 - To avoid having required inputs styled as invalid before user's interaction, I used JavaScript's "onblur" method to create a
+new class that contains invalid styling;
+4 - I used a 'keydown' listener to mask the phone number;
+5 - To make "confirm password" work, I used a keyup listener and a couple of methods to transform the inputs class and id;
+
+PT-BR
+
+O objetivo deste projeto é construir um modelo de página de inscrição. Seu tema é um campeonato imaginário de xadrez;
+
+1 - Usei flexbox para organizar a estrutura geral da página, configurando à esquerda o logo e a imagem de backgroud, enquanto o painel de interação do usuário ficou à direita;
+2 - Posicionamento "relative" foi usado para ajustar a altura do logo na página;
+3 - Para evitar que os inputs obrigatórios fossem estilizados como invalidos antes da interação do usuário, usei o método "onblur" do JavaScript para criar uma nova classe que contém o estilo dos inputs inválidos;
+4 - Usei um listener de 'keydown' para criar a máscara do número de telefone;
+5 - Para fazer funcionar o "confirm password", usei um listener de "keyup" e alguns métodos para transformar a classe e o is dos inputs;
--- script.js
+++ script.js
@@ -1,9 +1,58 @@
-//Mascara do telefone
-//Linkar o parâmetro do confirm password com a password
+//Fazer aviso de não igualdade entre senhas
 
-const required = document.querySelectorAll('input[required]');
+//Masks phone number
+const phoneNum = document.querySelector('#phone');
+phoneNum.addEventListener('keydown', () => {
+    num = phoneNum.value;
+    if(num.length == 1)
+    {
+        num = '(' + num;
+    }
+    else if(num.length == 4)
+    {
+        num = num + ')' + ' ';
+    }
+    else if(num.length == 9)
+    {
+        num = num + '-';
+    }
 
-
-required.forEach((required) => {
-    required.onblur = function(){ this.classList.add('visitado')}
+    phoneNum.value = num;
 });
+
+//Allows invalid style only after input loses focus
+const required = document.querySelectorAll('input[required]');
+required.forEach((required) => {
+    required.onblur = function(){ this.classList.add('visited')}
+});
+
+//Match passwords
+const password = document.querySelector('#pwd');
+const check = document.querySelector('#checkPwd');
+const msg = document.querySelector('.errorMsg');
+
+check.addEventListener('keyup', () => {
+    if(check.value === '')
+    {
+        msg.textContent='Enter a password';
+    }
+    else if(check.value == password.value)
+    {
+        check.classList.remove('error');
+        check.classList.add('ok');
+        password.classList.remove('error');
+        password.classList.add('ok');
+        msg.setAttribute('id', 'match');
+        msg.textContent= "OK!";
+    }
+    else if(check.value != password.value)
+    {
+        check.classList.add('error');
+        check.classList.remove('ok');
+        password.classList.add('error');
+        password.classList.remove('ok');
+        msg.removeAttribute('id', 'match');
+        msg.textContent= "Passwords do not match";
+    }
+});
+
